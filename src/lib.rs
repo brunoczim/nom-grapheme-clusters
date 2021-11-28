3d7b52a5c96f9b0c6d4519c8729ed90b266da485
brunoczim <brunoczim@gmail.com>
2021-11-28 19:29:44 -0300

    re-exporting internal items

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 //! This crate provides location for `nom` with support to grapheme clusters.
 
 pub mod source;
-pub mod location;
+mod location;
 pub mod span;
+
+pub use location::{LocatedSegment, Location};
+pub use source::Source;
+pub use span::{Span, SpanContent};
