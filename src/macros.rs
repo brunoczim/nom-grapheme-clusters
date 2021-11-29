8d662ad03c9420b6d4a767d44ea2671bc21ac76e
brunoczim <brunoczim@gmail.com>
2021-11-29 02:28:16 -0300

    examples for macro

--- src/macros.rs
+++ src/macros.rs
@@ -1,6 +1,52 @@
 //! This module defines macros.
 
 /// Creates a table of tags to be used e.g. in a parser.
+///
+/// # Examples
+///
+/// ## Binding
+///
+/// ```
+/// use nom_grapheme_clusters::tag_table;
+///
+/// # fn main() {
+/// tag_table! {
+///     name = "MyTags";
+///     let foo = "Foo";
+///     let bar = "Bar";
+/// }
+///
+/// assert_eq!(foo.as_str(), "Foo");
+/// assert_eq!(foo.start().position(), 0);
+/// assert_eq!(bar.as_str(), "Bar");
+/// assert_eq!(bar.start().position(), 3);
+/// # }
+/// ```
+///
+/// ## Struct
+///
+/// ```
+/// use nom_grapheme_clusters::{Span, tag_table};
+///
+/// struct MyTags {
+///     foo: Span,
+///     bar: Span,
+/// }
+///
+/// # fn main() {
+/// let tags = tag_table! {
+///     MyTags {
+///         foo: "Foo",
+///         bar: "Bar",
+///     }
+/// };
+///
+/// assert_eq!(tags.foo.as_str(), "Foo");
+/// assert_eq!(tags.foo.start().position(), 0);
+/// assert_eq!(tags.bar.as_str(), "Bar");
+/// assert_eq!(tags.bar.start().position(), 3);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! tag_table {
     ($(let $var:ident = $value:expr;)*) => {
