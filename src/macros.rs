3b707be1bd8318b54acfc9365e7aba0066f986df
brunoczim <brunoczim@gmail.com>
2021-11-29 11:19:07 -0300

    allow tag table name with struct

--- src/macros.rs
+++ src/macros.rs
@@ -11,11 +11,12 @@
 ///
 /// # fn main() {
 /// tag_table! {
-///     name = "MyTags";
+///     name = "foobar tags";
 ///     let foo = "Foo";
 ///     let bar = "Bar";
 /// }
 ///
+/// assert_eq!(foo.source().name(), "foobar tags");
 /// assert_eq!(foo.as_str(), "Foo");
 /// assert_eq!(foo.start().position(), 0);
 /// assert_eq!(bar.as_str(), "Bar");
@@ -35,12 +36,40 @@
 ///
 /// # fn main() {
 /// let tags = tag_table! {
+///     name = "foobar tags";
 ///     MyTags {
 ///         foo: "Foo",
 ///         bar: "Bar",
 ///     }
 /// };
 ///
+/// assert_eq!(tags.foo.source().name(), "foobar tags");
+/// assert_eq!(tags.foo.as_str(), "Foo");
+/// assert_eq!(tags.foo.start().position(), 0);
+/// assert_eq!(tags.bar.as_str(), "Bar");
+/// assert_eq!(tags.bar.start().position(), 3);
+/// # }
+/// ```
+///
+/// ## Using Struct Name as Source Code Name
+///
+/// ```
+/// use nom_grapheme_clusters::{Span, tag_table};
+///
+/// struct MyTags {
+///     foo: Span,
+///     bar: Span,
+/// }
+///
+/// # fn main() {
+/// let tags = tag_table! {
+///     MyTags {
+///         foo: "Foo",
+///         bar: "Bar",
+///     }
+/// };
+///
+/// assert_eq!(tags.foo.source().name(), "MyTags");
 /// assert_eq!(tags.foo.as_str(), "Foo");
 /// assert_eq!(tags.foo.start().position(), 0);
 /// assert_eq!(tags.bar.as_str(), "Bar");
@@ -83,14 +112,31 @@ macro_rules! tag_table {
         tag_table! { name = "<tags>"; $(let $var = $value;)* }
     };
 
-    ($name:ident { $($var:ident : $value:expr),* }) => {{
-        tag_table! { name = stringify!($name); $(let $var = $value;)* }
-        $name { $($var),* }
+    (
+        name = $name:expr; $struct_name:ident { $($var:ident : $value:expr),* }
+    ) => {{
+        tag_table! { name = $name; $(let $var = $value;)* }
+        $struct_name { $($var),* }
     }};
 
-    ($name:ident { $($var:ident : $value:expr,)* }) => {{
-        tag_table! { name = stringify!($name); $(let $var = $value;)* }
-        $name { $($var),* }
+    (
+        name = $name:expr; $struct_name:ident { $($var:ident : $value:expr,)* }
+    ) => {{
+        tag_table! { name = $name; $(let $var = $value;)* }
+        $struct_name { $($var),* }
     }};
 
+    ($struct_name:ident { $($var:ident : $value:expr),* }) => {{
+        tag_table! {
+            name = stringify!($struct_name);
+            $struct_name { $($var : $value),* }
+        }
+    }};
+
+    ($struct_name:ident { $($var:ident : $value:expr,)* }) => {{
+        tag_table! {
+            name = stringify!($struct_name);
+            $struct_name { $($var : $value),* }
+        }
+    }};
 }
